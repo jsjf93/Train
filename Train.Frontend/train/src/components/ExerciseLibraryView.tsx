1c258eb2e6422a18996152f4510d119c9489672a
jsjf93 <jsjf93@hotmail.co.uk>
2019-04-15 23:28:25 +0100

    Basic sorting implemented

--- Train.Frontend/train/src/components/ExerciseLibraryView.tsx
+++ Train.Frontend/train/src/components/ExerciseLibraryView.tsx
@@ -19,14 +19,16 @@ interface IProps {
 }
 
 interface IState {
-    exerciseList: Exercise[]
+    exerciseList: Exercise[];
+    sortDirection: string;
 }
 
 class ExerciseLibraryView extends Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
         this.state = {
-            exerciseList: []
+            exerciseList: [],
+            sortDirection: 'asc'
         }
     }
 
@@ -45,12 +47,44 @@ class ExerciseLibraryView extends Component<IProps, IState> {
             }
             const exerciseList: Exercise[] = [];
             data.map((row: Exercise) => exerciseList.push(row))
-            this.setState({ exerciseList });
+            this.setState({ 
+                exerciseList: exerciseList.sort(this.sortAscending('name')) 
+            });
             console.log("[ExerciseLibraryView] fetch. ", this.state.exerciseList);
         })
         // temporary code that just adds in test data hardcoded at top of file
         if (this.state.exerciseList.length == 0) 
-            this.setState({ exerciseList: data });
+            this.setState({ 
+                exerciseList: data.sort(this.sortAscending('name')) 
+            });
+    }
+
+    private sortAscending = (column: string) => {
+        return function (a: any, b: any) {
+            if (a[column] < b[column]) return -1;
+            if (a[column] > b[column]) return 1;
+            return 0;
+        };
+    }
+
+    private sortDescending = (column: string) => {
+        return function (a: any, b: any) {
+            if (a[column] > b[column]) return -1;
+            if (a[column] < b[column]) return 1;
+            return 0;
+        };
+    }
+
+    private sortBy = (column: string) => {
+        const exerciseList = this.state.exerciseList;
+        if (this.state.sortDirection === 'asc') {
+            exerciseList.sort(this.sortAscending(column));
+            this.setState({ exerciseList, sortDirection: 'desc' });
+        }
+        if (this.state.sortDirection === 'desc') {
+            exerciseList.sort(this.sortDescending(column));
+            this.setState({ exerciseList, sortDirection: 'asc' });
+        }
     }
 
     render() {
@@ -59,8 +93,8 @@ class ExerciseLibraryView extends Component<IProps, IState> {
                 <Table striped bordered hover variant="dark" size="sm">
                     <thead>
                         <tr>
-                            <td>Name</td>
-                            <td>Notes</td>                            
+                            <td onClick={() => this.sortBy('name')}>Name</td>
+                            <td onClick={() => this.sortBy('notes')}>Notes</td>                            
                         </tr>
                     </thead>
                     <tbody>
