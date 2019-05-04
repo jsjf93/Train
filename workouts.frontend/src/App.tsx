0e476388b1c3a2449ca67700cbc794f60b96c8c0
jsjf93 <jsjf93@hotmail.co.uk>
2019-05-04 14:09:49 +0100

    Initial commit for workouts frontend

---
+++ workouts.frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  public render() {
+    return (
+      <div className='App'>
+        <div className='app-content'>
+          <div className='workout-library-container'>
+            <h3>Workouts</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
