c549cb8dc9d8171c20706354002862caedb9ed6c
jsjf93 <jsjf93@hotmail.co.uk>
2019-05-11 08:45:57 +0100

    Workout list cards updated and formatted

--- workouts.frontend/src/components/WorkoutLibraryView.tsx
+++ workouts.frontend/src/components/WorkoutLibraryView.tsx
@@ -21,6 +21,53 @@ const data: IWorkout[] = [
             { id: 3, name: 'Plank', exerciseType: ExerciseType.Duration }
         ],
     },
+    {
+        id: 3,
+        name: 'Leg Day',
+        exercises: [
+            { id: 1, name: 'Squats', exerciseType: ExerciseType.Strength },
+            { id: 2, name: 'Running', exerciseType: ExerciseType.Duration },
+            { id: 3, name: 'Bulgarian Split Squats', exerciseType: ExerciseType.Strength }
+        ],
+    },
+    {
+        id: 4,
+        name: 'Bouldering A',
+        exercises: [
+            { id: 1, name: '4x4 Boulders', exerciseType: ExerciseType.Interval },
+            { id: 2, name: 'Max Strength Hangboarding', exerciseType: ExerciseType.Interval }
+        ],
+    },
+    {
+        id: 5,
+        name: 'Bouldering B',
+        exercises: [
+            { id: 1, name: 'Lead Climbing', exerciseType: ExerciseType.Interval },
+            { id: 2, name: 'Power Endurance Hangboarding', exerciseType: ExerciseType.Interval }
+        ],
+    },
+    {
+        id: 6,
+        name: 'Day 1',
+        exercises: [
+            { id: 1, name: 'Weighted Pullups', exerciseType: ExerciseType.Strength },
+            { id: 2, name: 'OHP', exerciseType: ExerciseType.Strength },
+            { id: 3, name: 'Dumbbell Rows', exerciseType: ExerciseType.Strength },
+            { id: 4, name: 'Reverse Flyes', exerciseType: ExerciseType.Strength },
+            { id: 5, name: 'Seated Curls', exerciseType: ExerciseType.Strength },
+        ],
+    },
+    {
+        id: 7,
+        name: 'Day 2',
+        exercises: [
+            { id: 1, name: 'Incline Bench Press', exerciseType: ExerciseType.Strength },
+            { id: 2, name: 'Barbell Rows', exerciseType: ExerciseType.Strength },
+            { id: 3, name: 'Dips', exerciseType: ExerciseType.Strength },
+            { id: 4, name: 'Lateral Raises', exerciseType: ExerciseType.Strength },
+            { id: 5, name: 'Tricep Extensions', exerciseType: ExerciseType.Strength },
+        ],
+    },
 ];
 
 interface IProps {
--- workouts.frontend/src/components/WorkoutList.tsx
+++ workouts.frontend/src/components/WorkoutList.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { IWorkout } from './index';
-import { Card } from 'react-bootstrap';
+import { IWorkout, IExercise } from './index';
+import { Card, ButtonGroup, Button } from 'react-bootstrap';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import './WorkoutList.css';
 
@@ -15,18 +15,42 @@ class WorkoutList extends Component<IProps, IState> {
     public render() {
         return (
             <div className="workout-list-container">
-                {this.props.workoutList.map(workout =>
-                    <Card className="workout-card" bg="dark" text="white" style={{ width: '18rem' }}>
+                {this.props.workoutList.map((workout, id) =>
+                    <Card 
+                        key={id} 
+                        className="workout-card" 
+                        bg="dark" 
+                        text="white"
+                    >
                         <Card.Header>{workout.name}</Card.Header>
                         <Card.Body>
-                            {workout.exercises.map(exercise =>
-                                <Card.Text><p>{exercise.name}</p></Card.Text>
+                            {this.renderFirstThreeExercises(workout.exercises).map((exercise, id) =>
+                                <Card.Text key={id}>{exercise.name}</Card.Text>
                             )}
+                            {<Card.Text>...</Card.Text>}
                         </Card.Body>
+                        <Card.Footer className="card-footer">
+                            <ButtonGroup>
+                                <Button variant="secondary">View</Button>
+                                <Button variant="success">Start</Button>
+                            </ButtonGroup>
+                        </Card.Footer>
                     </Card>
                 )}
             </div>
         );
+    }
+
+    private renderFirstThreeExercises(exercises: IExercise[]): IExercise[] {
+        if (exercises.length < 3) {
+            return exercises;
+        }
+
+        let firstThreeExercises: IExercise[] = [];
+        for (let i = 0; i < 3; i++) {
+            firstThreeExercises.push(exercises[i]);
+        }
+        return firstThreeExercises;
     }
 }
 
