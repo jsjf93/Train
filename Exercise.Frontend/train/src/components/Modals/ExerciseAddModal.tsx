5c80a0900ed6984d7836cf6d29a9dab1c0c43cd4
jsjf93 <jsjf93@hotmail.co.uk>
2019-05-25 15:48:33 +0100

    Updated folder names

---
+++ Exercise.Frontend/train/src/components/Modals/ExerciseAddModal.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Form, Button, Modal } from '../../../node_modules/react-bootstrap';
+
+interface IProps {
+    showExerciseModal: boolean;
+    handleClose: () => void;
+    exerciseNameInput: () => void;
+    addExercise: () => void;
+}
+
+const ExerciseAddModal = (props: IProps) => {
+    return (
+        <Modal show={props.showExerciseModal} onHide={props.handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add Exercise</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Exercise Name</Form.Label>
+                        <Form.Control
+                            type='text'
+                            ref={props.exerciseNameInput}
+                        />
+                    </Form.Group>
+                </Form>
+                <Button
+                    variant='success'
+                    type='submit'
+                    onClick={props.addExercise}
+                >
+                    Add Exercise
+                </Button>
+            </Modal.Body>
+        </Modal>
+    );
+};
+
+export default ExerciseAddModal;
