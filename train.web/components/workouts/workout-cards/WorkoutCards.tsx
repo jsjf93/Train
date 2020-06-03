59eb047dd86c696d1b9639947758e2e6cf8e436c
Joshua Foster <jsjf93@hotmail.co.uk>
2020-06-03 16:58:46 +0100

    Styling cards

--- train.web/components/workouts/workout-cards/WorkoutCards.tsx
+++ train.web/components/workouts/workout-cards/WorkoutCards.tsx
@@ -11,33 +11,30 @@ interface IProps {
 
 const WorkoutCards = (props: IProps) => {
   return (
-    <CardDeck>
+    <CardDeck className={styles.cards__container}>
       {props.workouts.map(w => (
-        <Link key={w.id} href={"/workouts/[id]"} as={`/workouts/${w.id}`}>
-          <Card className={styles.card}>
-            <Card.Body>
-              <Card.Title className={styles.card__titleContainer}>
-                {w.workoutName}
-                <span className={styles.card__closeButton} onClick={() => props.deleteWorkout(w.id)}>
+        <Card key={w.id} className={styles.card}>
+          <Card.Body>
+            <Card.Title className={styles.card__titleContainer}>
+              {w.workoutName}
+              <Dropdown className={styles.card__menuButton}>
+                <Dropdown.Toggle id={`more-options-toggle-${w.id}`} className={styles.dropdownToggle}>
                   <MoreVert />
-                </span>
-                <Dropdown>
-                  <Dropdown.Toggle id={`more-options-toggle-${w.id}`} className={styles.dropdownToggle}>
-                    <MoreVert />
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>Delete</Dropdown.Item>
-                    <Dropdown.Item>Edit</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Card.Title>
-              <Card.Text>
-                <span className={styles.info}>Exercises; </span>
-                {w.workoutExercises.map(w => w.exerciseName).join(', ')}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Link>
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={() => props.deleteWorkout(w.id)}>Delete</Dropdown.Item>
+                  <Dropdown.Item as="button">
+                    <Link href={"/workouts/[id]"} as={`/workouts/${w.id}`}><a>Edit</a></Link>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Card.Title>
+            <Card.Text>
+              <span className={styles.info}>Exercises; </span>
+              {w.workoutExercises.map(w => w.exerciseName).join(', ')}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       ))}
     </CardDeck>
   )
